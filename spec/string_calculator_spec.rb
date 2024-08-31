444a83f46c8be43ab35572856f410de7d6e47e1e
kajal saini <sainikajal440@gmail.com>
2024-08-31 16:57:17 +0530

    Add test for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -26,6 +26,11 @@ RSpec.describe StringCalculator do
   	expect(StringCalculator.add("//;\n1;2")).to eq(3)
 	end
 
+	it 'raises an error for negative numbers' do
+  	expect { StringCalculator.add("1,-2,3") }.to raise_error("Negative numbers not allowed: -2")
+	end
+
+
 
 
 end
