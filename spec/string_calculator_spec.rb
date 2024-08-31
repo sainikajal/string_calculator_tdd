9723e8aa791fd9c8474d1ef158bec532027c82a7
kajal saini <sainikajal440@gmail.com>
2024-08-31 16:49:16 +0530

    Add test for an empty string

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require_relative '../src/string_calculator'
+
+RSpec.describe StringCalculator do
+  it 'returns 0 for an empty string' do
+    expect(StringCalculator.add("")).to eq(0)
+  end
+end
