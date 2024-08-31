e0d56d65302d7b8d12b8c1f0a9d47c6c434205f8
kajal saini <sainikajal440@gmail.com>
2024-08-31 16:53:54 +0530

    Handle two numbers separated by a comma

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -1,6 +1,8 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i
+    return numbers.to_i if numbers.split(",").length == 1
+
+    numbers.split(",").map(&:to_i).sum
   end
 end
